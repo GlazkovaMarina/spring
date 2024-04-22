2fdc6576779d67578d2493f5262db7f9fa477da7
GlazkovaMarina <glazkova.mv@bk.ru>
2024-04-22 10:26:48 +0300

    with postgresql

---
+++ Seminar05/hw_sem5/src/test/java/ru/gb/hw_sem5/HwSem5ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.hw_sem5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwSem5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
