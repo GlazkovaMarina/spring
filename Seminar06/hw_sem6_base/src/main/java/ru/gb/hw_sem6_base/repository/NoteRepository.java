12bd491957ed0133c97ed1c83546df6b268655b6
GlazkovaMarina <98487855+GlazkovaMarina@users.noreply.github.com>
2024-04-23 09:51:04 +0300

    Delete Seminar06/hw_sem6_base directory

--- Seminar06/hw_sem6_base/src/main/java/ru/gb/hw_sem6_base/repository/NoteRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.gb.hw_sem6_base.repository;
-import org.springframework.stereotype.Repository;
-import ru.gb.hw_sem6_base.domain.Note;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@Repository
-public interface NoteRepository extends JpaRepository<Note, Long>{
-}
