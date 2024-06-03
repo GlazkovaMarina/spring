189b8a5e591a7e7c09ab7f5bc4d1bc7b2fe92054
GlazkovaMarina <glazkova.mv@bk.ru>
2024-06-03 17:04:44 +0300

    add hw12

---
+++ hw12/src/main/java/ru/gb/hw12/service/FileGateAway.java
@@ -0,0 +1,11 @@
+package ru.gb.hw12.service;
+
+
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.integration.file.FileHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+
+@MessagingGateway(defaultRequestChannel = "TextInputChannel")
+public interface FileGateAway {
+    void writeToFile(@Header(FileHeaders.FILENAME) String fileName, String data);
+}
